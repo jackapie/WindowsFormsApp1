70e8bd0149a740bc85192760ceee22e8e896a3ca
Jackie Pearce <jackiepearce83@hotmail.com>
2019-06-23 21:57:42 +0100

    Made a simple summing app

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class frmSummingApp
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbx1
+            // 
+            this.tbx1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbx1.Location = new System.Drawing.Point(114, 35);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(100, 20);
+            this.tbx1.TabIndex = 0;
+            // 
+            // tbx2
+            // 
+            this.tbx2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbx2.Location = new System.Drawing.Point(432, 35);
+            this.tbx2.Multiline = true;
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(100, 101);
+            this.tbx2.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(457, 302);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmSummingApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbx2);
+            this.Controls.Add(this.tbx1);
+            this.Name = "frmSummingApp";
+            this.Text = "Summing App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class frmSummingApp : Form
     {
-        public Form1()
+        public frmSummingApp()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    int answer = int.Parse(tbx1.Text) + int.Parse(tbx2.Text);
+            //    MessageBox.Show(answer.ToString());
+            //}
+
+            //catch
+            //{
+            //    MessageBox.Show("An error occurred. Please enter a number into each box.");
+            //}
+
+            if (int.TryParse(tbx1.Text, out int num1) && int.TryParse(tbx2.Text, out int num2)) {
+                MessageBox.Show((num1 + num2).ToString());
+
+            }
         }
     }
 }
